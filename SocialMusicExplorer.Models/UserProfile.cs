0a50e04e749696ec8c4716cfaba44fad8c75f938
Adam O'Neil <adamosoftware@gmail.com>
2018-10-25 16:14:22 -0400

    user profile started

---
+++ SocialMusicExplorer.Models/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using Postulate.Lite.Core.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialMusicExplorer.Models
+{
+	[Table("AspNetUsers")]
+	[Identity(nameof(UserId), constraintName: "U_AspNetUsers_UserId")]
+	public class UserProfile
+	{
+		public int UserId { get; set; }
+
+		[Required]
+		public string UserName { get; set; }
+
+		[DefaultExpression("0")]
+		public int TimeZoneOffset { get; set; }
+	}
+}
